460e86bfd95528dc9f77123a7eb487acd55b9419
gerardonet <gerardo@netcommerce.mx>
2025-07-23 17:21:33 -0600

    Update whatsapp-widget-purificadoras.js

--- whatsapp-widget-purificadoras.js
+++ whatsapp-widget-purificadoras.js
@@ -1,7 +1,7 @@
 (function () {
   const scripts = document.querySelectorAll('script[src*="whatsapp-widget"]');
   const script = scripts[scripts.length - 1];
-  const numeroWhatsApp = script?.getAttribute('data-whatsapp') || '5213314065222';
+  const numeroWhatsApp = script?.getAttribute('data-whatsapp') || '523336308620';
   const zapierURL = script?.getAttribute('data-zapier') || '';
 
   const style = document.createElement('style');
